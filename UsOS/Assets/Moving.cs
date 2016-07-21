23adb741540186b2a7d1eea055ee55727972626c
bojo <stil.2910@gmail.com>
2016-07-21 13:19:35 +0300

    Initial commit in the new organisational repo

---
+++ UsOS/Assets/Moving.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Moving : MonoBehaviour
+{
+    public int sprintingSpeed = 5;
+
+    private float horizontal;
+    private Animator animator;
+    private bool isSprintig;
+
+    public void Awake()
+    {
+        this.animator = this.GetComponent<Animator>();
+    }
+
+
+    void FixedUpdate()
+    {
+        horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+        MovingCharacter();
+
+    }
+
+    private void MovingCharacter()
+    {
+        if (horizontal <= 0.5 && this.horizontal > 0)
+        {
+            this.animator.SetTrigger("Walking");
+            this.transform.Translate(Vector3.right * Time.deltaTime);
+            this.gameObject.GetComponent<SpriteRenderer>().flipX = false;
+
+        }
+        else if (this.horizontal > 0.5)
+        {
+            this.isSprintig = true;
+            this.animator.SetTrigger("Sprint");
+            this.transform.Translate(Vector3.right * sprintingSpeed * Time.deltaTime);
+            this.gameObject.GetComponent<SpriteRenderer>().flipX = false;
+
+        }
+        else if (this.horizontal >= -0.5 && this.horizontal < 0)
+        {
+            this.animator.SetTrigger("Walking");
+            this.transform.Translate(Vector3.left * Time.deltaTime);
+            this.gameObject.GetComponent<SpriteRenderer>().flipX = true;
+
+        }
+        else if (this.horizontal < -0.5)
+        {
+            this.animator.SetTrigger("Sprint");
+            this.transform.Translate(Vector3.left * sprintingSpeed * Time.deltaTime);
+            this.gameObject.GetComponent<SpriteRenderer>().flipX = true;
+
+        }
+        else
+        {
+            this.animator.SetTrigger("Idle");
+        }
+    }
+}
---
+++ UsOS/Assets/Scripts/ResizeBackground.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ResizeBackground : MonoBehaviour
+{
+    public void OnValidate()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr == null) return;
+
+        this.transform.localScale = new Vector3(1, 1, 1);
+
+        float width = sr.sprite.bounds.size.x;
+        float height = sr.sprite.bounds.size.y;
+
+        float worldScreenHeight = Camera.main.orthographicSize * 2.0f;
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        this.transform.localScale = new Vector2(worldScreenWidth / width, worldScreenHeight / height);
+    }
+}
