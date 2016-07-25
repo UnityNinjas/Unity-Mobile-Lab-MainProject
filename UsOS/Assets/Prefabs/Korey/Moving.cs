15d7a04b464d6d2e8d6cf75074987cebb97359eb
Mihail-Madzharov <mihail.madzharov.m@gmail.com>
2016-07-25 23:13:49 +0300

    Korey and guard bot

---
+++ UsOS/Assets/Prefabs/Korey/Moving.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Moving : MonoBehaviour
+{
+    public int sprintingSpeed = 1;
+
+    private float horizontal;
+    private Animator animator;
+    private bool isSprintig;
+    private bool isJumping;
+    private bool endOfAnimation;
+    private bool lowerKick;
+    private Rigidbody2D rb;
+
+    public void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        this.animator = this.GetComponent<Animator>();
+    }
+
+
+
+    void FixedUpdate()
+    {
+        horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+
+        MovingCharacter();
+
+        if (endOfAnimation)
+        {
+            this.animator.SetTrigger("Idle");
+            this.endOfAnimation = false;
+        }
+    }
+
+    private void MovingCharacter()
+    {
+        if (!this.isJumping)
+        {
+            if (horizontal <= 0.5 && this.horizontal > 0)
+            {
+                this.animator.SetTrigger("Walking");
+                this.transform.Translate(Vector3.right * Time.deltaTime);
+                this.gameObject.GetComponent<SpriteRenderer>().flipX = false;
+
+            }
+            else if (this.horizontal > 0.5)
+            {
+                this.isSprintig = true;
+                this.animator.SetTrigger("Sprint");
+                this.transform.Translate(Vector3.right * sprintingSpeed * Time.deltaTime);
+                this.gameObject.GetComponent<SpriteRenderer>().flipX = false;
+
+            }
+            else if (this.horizontal >= -0.5 && this.horizontal < 0)
+            {
+                this.animator.SetTrigger("Walking");
+                this.transform.Translate(Vector3.left * Time.deltaTime);
+                this.gameObject.GetComponent<SpriteRenderer>().flipX = true;
+
+            }
+            else if (this.horizontal < -0.5)
+            {
+                this.isSprintig = true;
+                this.animator.SetTrigger("Sprint");
+                this.transform.Translate(Vector3.left * sprintingSpeed * Time.deltaTime);
+                this.gameObject.GetComponent<SpriteRenderer>().flipX = true;
+
+            }
+            else
+            {
+                this.animator.SetTrigger("Idle");
+
+            }
+        }
+
+        this.isSprintig = false;
+    }
+
+    public void EndOfLowerKick()
+    {
+        this.lowerKick = false;
+        this.endOfAnimation = true;
+    }
+
+    public void Punch()
+    {
+        if (isJumping)
+        {
+            this.animator.SetTrigger("AirPunch");
+        }
+        else
+        {
+            this.animator.SetTrigger("Punch");
+        }
+    }
+
+    public void OnCollisionExit2D(Collision2D hitObject)
+    {
+        if (hitObject.gameObject.tag == "Korey" && hitObject.transform.position.y < this.transform.position.y)
+        {
+            this.isJumping = true;
+
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D hitObject)
+    {
+        if (hitObject.gameObject.name.Equals("Floor"))
+        {
+            this.isJumping = false;
+        }
+
+        this.animator.SetTrigger("Idle");
+
+    }
+
+    public void Jump()
+    {
+        //It's better with rigidbody than translate
+        //   this.transform.Translate(Vector3.up * 20 * Time.deltaTime);
+        this.rb.AddForce(Vector3.up * 5, ForceMode2D.Impulse);
+        this.animator.SetTrigger("Jump");
+
+    }
+
+
+    public void Kick()
+    {
+        if (isSprintig)
+        {
+            this.animator.SetTrigger("LowerKick");
+            lowerKick = true;
+        }
+        else
+        {
+            this.animator.SetTrigger("NormalKick");
+        }
+    }
+}
---
+++ UsOS/Assets/Scripts/WalkingGurad.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+public class WalkingGurad : MonoBehaviour
+{
+    private RectTransform currenTransform;
+    private float maxPosition;
+    private bool isFocused;
+    private int speed = 2;
+    private Animator animator;
+    private SpriteRenderer sprite;
+
+    void Start()
+    {
+        this.currenTransform = GetComponent<RectTransform>();
+
+        this.sprite = this.GetComponent<SpriteRenderer>();
+
+        this.animator = this.GetComponent<Animator>();
+
+        this.maxPosition = this.currenTransform.localPosition.x;
+    }
+
+    void FixedUpdate()
+    {
+
+        MovingGuard();
+        ClampPosition();
+    }
+
+    private void MovingGuard()
+    {
+        if (!isFocused)
+        {
+            this.currenTransform.Translate(Vector3.right * this.speed * Time.deltaTime);
+
+        }
+    }
+
+
+    public void Shoot()
+    {
+        this.animator.SetTrigger("Shoot");
+    }
+
+    private void ClampPosition()
+    {
+        Debug.Log(this.maxPosition);
+        if (!isFocused)
+        {
+            this.animator.SetTrigger("Walking");
+
+            if (currenTransform.localPosition.x < -this.maxPosition)
+            {
+                this.speed *= -1;
+                this.sprite.flipX = true;
+            }
+
+            if (currenTransform.localPosition.x > this.maxPosition)
+            {
+                this.speed *= -1;
+                this.sprite.flipX = false;
+            }
+
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Korey")
+        {
+            this.animator.SetTrigger("Idle");
+            this.isFocused = true;
+            this.sprite.flipX = false;
+
+        }
+    }
+
+
+
+    public void OnTriggerExit2D(Collider2D go)
+    {
+        if (go.gameObject.tag == "Korey")
+        {
+            this.isFocused = false;
+        }
+    }
+
+}
