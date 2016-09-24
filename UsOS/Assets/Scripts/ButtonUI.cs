66193dbeb41753cbfa10de6696e1200973ce14b1
kasskata <kassskata@gmail.com>
2016-09-24 16:03:38 +0300

    Clean project

---
+++ UsOS/Assets/Scripts/ButtonUI.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class ButtonUI : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField]
+    public UnityEvent OnClick;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        this.OnClick.Invoke();
+    }
+}
